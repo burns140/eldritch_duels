8ea5db810d4f0f64427bc48f7e75daa3245036a0
Stephen Burns <burns140@purdue.edu>
2020-02-05 18:45:58 -0500

    fixed compile error cuz _

--- server/api/decks.js
+++ server/api/decks.js
@@ -2,7 +2,7 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const router = express.Router();
 const assert = require('assert');
-const dbconfig = require('../db_config.json');
+const dbconfig = require('../dbconfig.json');
 const verify = require('../verifyjwt');
 
 router.use(express.urlencoded({extended: false}));
