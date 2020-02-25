ea4091ed9b9f8d298be3c97b6949f42ef143c744
nibarder <43069746+nibarder@users.noreply.github.com>
2020-02-25 08:45:36 -0500

    Added deck builder

---
+++ client/Eldritch Duels/Assets/scripts/cards/Deck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace eldritch.cards {
+    public class Deck
+    {
+        private string name = "";
+        public string DeckName
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        private List<CardContainer> deck;
+        public List<CardContainer> CardsInDeck
+        {
+            get { return this.deck; }
+            set { this.deck = value; }
+        }
+
+        public int DeckSize
+        {
+            get { return this.deck.Count; }
+        }
+
+    }
+}
---
+++ client/Eldritch Duels/Assets/scripts/cards/DeckMenuUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace eldritch.cards
+{
+    public class DeckMenuUI : MonoBehaviour
+    {
+        private void Start()
+        {
+#if DEBUG
+            testDeckUI();
+#endif
+
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void NewDeck()
+        {
+            Global.selectedDeck = null;
+            LoadScene("DeckBuilder");
+        }
+
+        private void testDeckUI()
+        {
+            Global.InitNewPlayer();
+        }
+    }
+}
