e823858a20d7862090a2e75f0362ddf48d47e7b7
cjmms <brandon.xyw@gmail.com>
2020-02-25 02:08:46 -0500

    Open Login Panel
    Add Login Button
    Open Login panel when clicking Login button

---
+++ client/Eldritch Duels/Assets/Scripts/PanelOpener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+
+    public GameObject Panel;
+    
+    public void OpenPanel()
+    {
+        if (Panel != null) 
+        {
+            Panel.SetActive(true);
+        }
+
+        
+    }
+}
