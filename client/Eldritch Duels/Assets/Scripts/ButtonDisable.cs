a23623ba90f282f0c789303f737b2e7932b4929f
cjmms <brandon.xyw@gmail.com>
2020-02-25 02:44:36 -0500

    Toggle Panel
    Add a cancel button to login panel
    cancel button disables login panel

---
+++ client/Eldritch Duels/Assets/Scripts/ButtonDisable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDisable : MonoBehaviour
+{
+    public GameObject Button;
+
+    public void DiableButton() 
+    {
+        if (Button != null) 
+        {
+            Button.SetActive(false);
+        }
+    }
+}
---
+++ client/Eldritch Duels/Assets/Scripts/PanelToggler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this class enables/disables Panel object
+
+public class PanelToggler : MonoBehaviour
+{
+    public GameObject Panel;
+    
+    public void TogglePanel()
+    {
+        if (Panel != null) 
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+}
