77e579f14e090b507121b7c9b191f97519e70976
cjmms <brandon.xyw@gmail.com>
2020-02-25 01:40:23 -0500

    Quit Button
    Implement Quit Button
    Add Button animation

--- client/Eldritch Duels/Assets/Scripts/ExitGame.cs
+++ client/Eldritch Duels/Assets/Scripts/ExitGame.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 
 public class ExitGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
     public void doExitGame()
     {
+        Debug.Log("Has quit Game");
         Application.Quit();
     }
 }
