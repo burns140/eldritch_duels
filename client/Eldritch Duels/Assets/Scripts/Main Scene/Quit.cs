b4c11c120f604fcf479ca6c7e90e97a02c83f0c6
cjmms <brandon.xyw@gmail.com>
2020-02-26 18:04:27 -0500

    Fix Quit Script

---
+++ client/Eldritch Duels/Assets/Scripts/Main Scene/Quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public void doExitGame()
+    {
+        Debug.Log("Has quit Game");
+        Application.Quit();
+    }
+}
