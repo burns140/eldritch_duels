619d4bd0550ed6f144df96adc65a5dfe9148d290
cjmms <brandon.xyw@gmail.com>
2020-02-26 20:28:04 -0500

    Change Profile scene
    Add two buttons

---
+++ client/Eldritch Duels/Assets/Scripts/Lobby Scene/GotoProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GotoProfile : MonoBehaviour
+{
+    public void MovetoProfile()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
