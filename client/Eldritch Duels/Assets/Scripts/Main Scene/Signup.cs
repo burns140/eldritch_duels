d999b525d3a15171b3910ac6c56b2a605db8e65f
Lumamaster <gao375@prudue.edu>
2020-02-27 22:26:51 -0500

    added matchmaking script

--- client/Eldritch Duels/Assets/Scripts/Main Scene/Signup.cs
+++ client/Eldritch Duels/Assets/Scripts/Main Scene/Signup.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using Newtonsoft.Json;
-using System.IO;
 using UnityEngine;
 using eldritch;
 
