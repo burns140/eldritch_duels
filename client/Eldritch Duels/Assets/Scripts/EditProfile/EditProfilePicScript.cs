d0d03ffe533497c83f0cd08b8a7fbebfea77ec8d
Lumamaster <gao375@prudue.edu>
2020-02-27 19:00:29 -0500

    fixed your imports

--- client/Eldritch Duels/Assets/Scripts/EditProfile/EditProfilePicScript.cs
+++ client/Eldritch Duels/Assets/Scripts/EditProfile/EditProfilePicScript.cs
@@ -1,15 +1,18 @@
 
 using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using eldritch;
 using UnityEngine.SceneManagement;
-
-
+using System.Collections.Generic;
 
 public class EditProfilePicScript : MonoBehaviour
 {
