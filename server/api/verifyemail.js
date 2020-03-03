43f858207472cd4ec681dc52faf9a7a0511fc831
Stephen Burns <burns140@purdue.edu>
2020-03-03 15:40:57 -0500

    forgot to branch for http server but I haven't changed any stuff that already works and server will still start so it will be fine

---
+++ server/api/verifyemail.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const router = express.Router();
+const dbconfig = require('../dbconfig.json');
+
+router.use(express.urlencoded({extended:false}));
+router.get('/:verify', async (req, res) => {
+    const verify = req.params.verify;
+
+    try {
+        MongoClient.connect(dbconfig.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+            assert.equal(null, err);
+            const db = client.db('eldritch_data');
+
+            db.collection('users').updateOne(
+                { verifyStr: verify },
+                { $set: { verifyStr: "verified", verified: true } }
+            ).then(result => {
+                if (result.modifiedCount != 1) {
+                    console.log('falied to verify email');
+                    res.status(400).send(`failed to verify email`);
+                } else {
+                    console.log('verified email');
+                    res.status(200).send('successfully verified email');
+                }
+                client.close();
+                return;
+            }).catch(err => {
+                console.log(err);
+                res.status(400).send(err);
+                client.close();
+                return;
+            });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).send("Could not verify email");
+    }
+});
+
+module.exports = router;
