7aade2b484edc35810039b3f64126d282fc047d4
dhairyaa <amind@purdue.edu>
2020-03-09 00:29:11 -0400

    Adding music functionality

---
+++ client/Eldritch Duels/Assets/Scripts/GlobalCheckMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCheckMusic : MonoBehaviour
+{
+    public AudioClip audioClip; // The audio clip supposed to be playing for the current scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Checking if music playing is correct");
+
+        if(LoadMusic.Instance.gameObject.GetComponent<AudioSource>().clip != audioClip){
+            LoadMusic.Instance.gameObject.GetComponent<AudioSource>().clip = audioClip; // Change clip
+            LoadMusic.Instance.gameObject.GetComponent<AudioSource>().Play(); // Play the new clip
+        }
+    }
+
+}
---
+++ client/Eldritch Duels/Assets/Scripts/Setting Scene/VolumeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeScript : MonoBehaviour
+{
+    public Slider slider; // Slider UI
+    private const string MUSIC_PREF_KEY = "music"; // PLAYER PREF KEY to store music volume
+
+    int wantedVolume; //= 5; //sample value to test
+
+    public void SetVolume(float Volume){
+       
+        AudioListener.volume = Volume; // Dynamically change volume according to slider
+        Debug.Log("Selected volume is: "+Volume);
+    }
+
+    void Start(){
+        wantedVolume=PlayerPrefs.GetInt(MUSIC_PREF_KEY); // Get the music volume from PLAYER PREFS
+        slider.value = wantedVolume; // Move slider on UI to the saved volume
+        Debug.Log("This is the saved volume: "+wantedVolume);
+    }
+
+    public void saveVolume(){
+        Debug.Log("New saved volume is: "+slider.value);
+        PlayerPrefs.SetFloat(MUSIC_PREF_KEY, slider.value); // Save new music volume to PlayerPref
+    }
+    public void cancelVolume(){
+        AudioListener.volume = wantedVolume; // Reset volume
+        slider.value = wantedVolume; // Reset slider position on UI
+    }
+    
+}
