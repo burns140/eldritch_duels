6b617e0905142a3faafe12ed79614a7f93b7ad21
Lumamaster <gao375@prudue.edu>
2020-03-09 23:30:50 -0400

    chat framework
    access it in duel scene

---
+++ client/Eldritch Duels/Assets/Scripts/DuelScene/Chat.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Chat;
+using ExitGames.Client.Photon;
+using eldritch;
+
+public class Chat : MonoBehaviour, IChatClientListener
+{
+    ChatClient chatClient;
+    string channelname = "testchannel"; //TODO: PASS IN CHANNEL NAME SOMEWHERE, POSSIBLY WHEN MATCHMAKING IS FOUND
+
+    public UnityEngine.UI.Button connectbutton;
+    public UnityEngine.UI.Button sendbutton;
+    public UnityEngine.UI.InputField sendtext;
+    public UnityEngine.UI.Text textbox;
+    private string messagetext;
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        if (level == ExitGames.Client.Photon.DebugLevel.ERROR)
+        {
+            Debug.LogError(message);
+        }
+        else if (level == ExitGames.Client.Photon.DebugLevel.WARNING)
+        {
+            Debug.LogWarning(message);
+        }
+        else
+        {
+            Debug.Log(message);
+        }
+    }
+
+    public void OnChatStateChange(ChatState state)
+    {
+        Debug.Log(state.ToString());
+    }
+
+    public void OnConnected()
+    {
+        chatClient.Subscribe(new string[] { channelname }); //REPLACE CHANNEL NAME WITH GAME ID OR SOMETHING
+    }
+
+    public void OnDisconnected()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        int msgCount = messages.Length;
+        textbox.text = "";
+        for (int i = 0; i < msgCount; i++)
+        { //go through each received msg
+            string sender = senders[i];
+            string msg = (string)(messages[i]);
+            Debug.Log(sender + ": " + msg);
+            textbox.text += sender + ": " + msg + "\n";
+        }
+    }
+
+    public void OnPrivateMessage(string sender, object message, string channelName) // We aren't planning on having private messages for now.
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message) // we don't have friends right now so this isn't helpful
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results) 
+    {
+        Debug.Log("Subscribed to a new channel!");
+    }
+
+    public void OnUnsubscribed(string[] channels)
+    {
+        Debug.Log("Unsubscribed from a channel!");
+    }
+
+    public void OnUserSubscribed(string channel, string user)
+    {
+        Debug.Log("New " + user + " joined " + channel + "!");
+    }
+
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        Debug.Log(user + " left the channel " + channel);
+    }
+
+    public void OnApplicationQuit()
+    {
+        if (chatClient != null) { chatClient.Disconnect(); }
+    }
+
+    public void connect()
+    {
+        Debug.Log("Connecting...");
+        ExitGames.Client.Photon.ConnectionProtocol connectProtocol = ExitGames.Client.Photon.ConnectionProtocol.Tcp;
+        this.chatClient = new ChatClient(this, connectProtocol) //Creates a chat client, and sets it to the specified region.
+        {
+            ChatRegion = "US" //SET CHAT REGION
+        };
+        Debug.Log("Attempted to create chat client.");
+
+        Photon.Chat.AuthenticationValues authValues = new Photon.Chat.AuthenticationValues
+        {
+            UserId = Global.username,
+            AuthType = Photon.Chat.CustomAuthenticationType.None
+        };
+
+        Debug.Log("Attempted to authenticate.");
+
+        chatClient.Connect("fb39f16f-bcce-4d9d-9910-db8c9abfd911", "0.01", authValues); //DO NOT CHANGE THIS
+
+        Debug.Log("Attempted to connect.");
+    }
+
+    public void sendMessage()
+    {
+        Debug.Log("Attempting to send message...");
+        chatClient.PublishMessage(channelname, messagetext);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        connectbutton.onClick.AddListener(connect);
+        sendbutton.onClick.AddListener(sendMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chatClient != null) {
+            chatClient.Service();
+        }
+        messagetext = sendtext.text;
+    }
+
+    
+}
