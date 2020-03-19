8c27d9409572b2632ddd8d5027281cfee55c0962
dhairyaa <amind@purdue.edu>
2020-03-19 17:31:09 -0500

    Update health, mana, & play button functionality

--- client/Eldritch Duels/Assets/Scripts/DuelScene/DuelScript.cs
+++ client/Eldritch Duels/Assets/Scripts/DuelScene/DuelScript.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using eldritch;
 using eldritch.cards;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class DuelScript : MonoBehaviour 
 {
+    #region UI & Script Variables
     // Cards in the deck UI
     public GameObject cardBack1; 
     public GameObject cardBack2;
     public GameObject cardBack3;
     public GameObject cardBack4;
 
-    public Text gameText;
+    public Text gameText; // Display game instructions and hints
     private bool isMyTurn; // Check if it is my turn
 
     public GameObject handAreaPanel; // Hand Area UI
@@ -26,20 +37,44 @@ public class DuelScript : MonoBehaviour
     private List<GameObject> myPlayList = new List<GameObject>(); // Store cards in my Play Area
     private List<GameObject> oppPlayList = new List<GameObject>(); // Store cards in opponent Play Area
 
-    public Button recallButton;
+    public Button recallButton; // Recall button on the UI
+    public GameObject myProfilePic; // My profile pic on the UI
+    public GameObject oppProfilePic; // Opponent's profile pic on the UI
 
+    public Text myManaText; // Text to show my current Mana
+    public Text oppManaText; // Text to show opponent's current Mana
+    public Text myHPText; // Text to show my current HP
+    public Text oppHPText; // Text to show opponent's current HP
+    public Image myHPImage; // Image to show my current HP
+
+    public Image oppHPImage; // Image to show opponent's current HP
+
+    public Sprite[] availablePictures; // Available profile pics
+    private float myCurrentHP; // My Current HP
+    private float oppCurrentHP; // Opp Current HP
+    private const float MAX_HEALTH = 30; // Max Health for a user
+    private const float MAX_MANA = 1; // Max Mana for a user
+
+    #endregion
+
+    #region Awake
     // Awake is called when the script instance is being loaded.
     void Awake(){
         setUpDeck(); // Set up card list from deck being used
+        setUpProfilePics(); // Set up profile pics for both users
+        setUpHealthMana(); // Set up health & mana to full for both users
         StartCoroutine(initCoroutines());
     }
+    #endregion
 
+    #region Update Functions
     // Update is called once per frame
     void Update()
     {
         checkDeckCount(); // Check & update card back quantity on deck UI
     }
 
+    // Check current number of cards in deck
     private void checkDeckCount(){
         if(deckList.Count<30){
             cardBack1.SetActive(false); // Hide first card in deck
@@ -54,7 +89,9 @@ public class DuelScript : MonoBehaviour
             cardBack4.SetActive(false); // Hide fourth/last card in deck
         }
     }
+    #endregion
 
+    #region Bookkeeping Before Playing
     IEnumerator initCoroutines(){
         yield return StartCoroutine(initalDraw()); // Set up hand to have 6 cards
         yield return StartCoroutine(testPlayArea()); // Test moving cards from hand to my play area
@@ -120,7 +157,49 @@ public class DuelScript : MonoBehaviour
             deckCount++; 
         }
     }
+    
+    // Set up health and mana text for both users to max values
+    private void setUpHealthMana(){
+        myHPText.text = MAX_HEALTH + " HP";
+        myManaText.text = MAX_MANA + " MANA";
+        oppHPText.text = MAX_HEALTH + " HP";
+        oppManaText.text = MAX_MANA + " MANA";
+    }
 
+    // Set up profile pics on the UI
+    private void setUpProfilePics(){
+        bool hasMyPicIndex=true; // Check if my pic is uploaded or has index (@KEVIN G)
+        bool hasOppPicIndex=true; // Check if opponent's pic is uploaded or has index (@KEVIN G)
+        int myPicIndex=0; // @TODO Get my profile pic index from server (@Stephen)
+        int oppPicIndex=1; // @TODO Get opponent's profile pic index from server (@Stephen)
+
+        if(hasMyPicIndex){
+            myProfilePic.GetComponent<Image>().sprite = availablePictures[myPicIndex]; // Set image from list
+        }
+        else{
+            // @TODO GET MY UPLOADED PICTURE FROM SERVER (@KEVIN G)
+            // Sprite newImage = ;
+            // myProfilePic.GetComponent<Image>().sprite = newImage;
+        }
+        myProfilePic.SetActive(true); // Unhide the image UI
+
+        if(hasOppPicIndex){
+            oppProfilePic.GetComponent<Image>().sprite = availablePictures[oppPicIndex]; // Set image from list
+        }
+        else{
+            // @TODO GET OPP UPLOADED PICTURE FROM SERVER (@KEVIN G)
+            // Sprite newImage = ;
+            // oppProfilePic.GetComponent<Image>().sprite = newImage;
+        }
+
+        oppProfilePic.SetActive(true); // Unhide the image UI
+
+        
+    }
+    
+    #endregion
+
+    #region Place Cards
     // Called at the beginning of every turn
     private void drawCard(){
         if(deckList.Count>0){
@@ -133,20 +212,60 @@ public class DuelScript : MonoBehaviour
         
     }
 
+    // Play opponent's card
+    private void playOppCard(){
+        
+    }
+    #endregion
+
+    #region Attack & Update Health
+    // Play card animations when I attack 
+    private void myAttack(){
+        foreach(GameObject value in myPlayList){
+            // @TODO some attack animations (@BRANDON)
+            float hit=0; // @TODO get attack value of the card (@DHAIRYA)
+            // @TODO send attack value to server (@KEVIN M)
+            updateOppHealth(hit); // After each card's attack, update opponent's health
+        }
+    }
+
+    // After my card attacks opponent
+    private void updateOppHealth(float hit){
+        oppCurrentHP -= hit; // Decrease attack from HP
+        oppHPImage.fillAmount = oppCurrentHP/MAX_HEALTH; // Update opponent's HP on UI
+    }
+
+    // After opponent's card attacks me
+    private void updateMyHealth(){
+        float hit=0; // @TODO get attack value from server (@KEVIN M)
+        myCurrentHP -= hit; // Decrease attack from HP
+        myHPImage.fillAmount = myCurrentHP/MAX_HEALTH; // Update my HP on UI
+    }
+    #endregion
+
+    #region End Turns
     // Check cards in hand
     private void checkHand(){
 
     }
 
     // End my turn
-    private void EndMyTurn(){
-
+    private void endMyTurn(){
         isMyTurn = false; // No longer my turn
+        string endString = "YOUR TURN"; // Send this to server
     }
 
-    // Play opponent's card
-    private void playOppCard(){
-        
+
+    // End opponent's play
+    private void endOppTurn(){
+        isMyTurn = true; // Now it's my turn
     }
-    
+    #endregion
+
+    #region End Game
+    // End game
+    private void endGame(){
+
+    }
+    #endregion
 }
