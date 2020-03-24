48ccb38806b01988e4bf96f2ecfe6bab0a7fcd04
nibarder <43069746+nibarder@users.noreply.github.com>
2020-03-24 12:43:34 -0400

    added 3 simple effects/abilities

---
+++ client/Eldritch Duels/Assets/Scripts/DuelScene/DuelFunctions.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using eldritch.cards;
+using eldritch;
+
+public static class DuelFunctions
+{
+    public static bool CanCast(Card castingcard, PlayerState ps){
+        if(castingcard.CardCost <= ps.mana){
+            return true;
+        }
+        return false;
+    }
+
+    public static bool CanBlock(Card attacker, Card blocker){
+        if(attacker.HasFly && !blocker.HasFly){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static List<Card> GetLibrary(){
+        Deck d = Global.selectedDeck;
+        if(d == null)
+            return null;
+        
+        List<Card> lib = new List<Card>();
+        foreach(CardContainer cc in d.CardsInDeck){
+            for(int i = 0; i < cc.count;i++){
+                lib.Add(cc.c);
+            }
+        }
+        return lib;
+    }
+
+    public static List<Card> ShuffleLibrary(List<Card> lib){
+        List<Card> newLib = new List<Card>();
+        while(lib.Count > 0){
+            int rn = Random.Range(0,lib.Count);
+            newLib.Add(lib[rn]);
+            lib.RemoveAt(rn);
+        }
+
+        return newLib;
+    }
+
+    public static Card DrawCard(ref PlayerState ps){
+        Card c = ps.library[0];
+        ps.library.RemoveAt(0);
+        return c;
+    }
+
+    public static Card RemoveFromHand(Card card, ref PlayerState ps){
+        Card c;
+        for(int i = 0; i < ps.inHand.Count;i++){
+            if(ps.inHand[i].CardName.Equals(card.CardName)){
+                c = ps.inHand[i];
+                ps.inHand.RemoveAt(i);
+                return c;
+            }
+            
+        }
+        return null;
+    }
+}
