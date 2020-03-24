f2285dc22128e3c3e744d0496c19e7dd33769720
nibarder <43069746+nibarder@users.noreply.github.com>
2020-03-24 12:30:05 -0400

    added bridge method for playing opp cards

--- client/Eldritch Duels/Assets/Scripts/DuelScene/DuelScript.cs
+++ client/Eldritch Duels/Assets/Scripts/DuelScene/DuelScript.cs
@@ -263,6 +263,14 @@ public class DuelScript : MonoBehaviour
         //TODO tell oppenent to resolve card
     }
 
+    //server sends request that opp played a card
+    //this method ties the server request with the
+    //client side
+    public void ServerPlayPlayOppCard(string cardName){
+        Card toPlay = Library.GetCard(cardName);
+        playOppCard(toPlay);
+    }
+
     // Play opponent's card
     private void playOppCard(Card played){
         OpponentState.onField.Add(played);
