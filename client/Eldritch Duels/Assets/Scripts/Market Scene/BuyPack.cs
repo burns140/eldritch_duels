4e077ea8303760263f9e7f773eaa5787925d82b1
cjmms <brandon.xyw@gmail.com>
2020-03-26 04:47:14 -0400

    Add script to Market scene
    Add a script to control panel
    Attached this script to all three buttons

---
+++ client/Eldritch Duels/Assets/Scripts/Market Scene/BuyPack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyPack : MonoBehaviour
+{
+    public GameObject succeedPanel;
+    public GameObject failedPanel;
+
+
+    public void buy() 
+    {
+        bool succeed = true;
+
+        if (succeed)
+            succeedPanel.SetActive(true);
+        else
+            failedPanel.SetActive(true);
+    }
+}
