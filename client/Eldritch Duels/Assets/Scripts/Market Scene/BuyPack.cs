6899506d9bf1feb9fa006aa96d479863760fe0bf
cjmms <brandon.xyw@gmail.com>
2020-03-26 05:13:00 -0400

    Update money text field
    Modify buyPack script
    this script should update money value from backend
    
    TODO:
    This script applys to all buttons, name of pack / price should passed into this function

--- client/Eldritch Duels/Assets/Scripts/Market Scene/BuyPack.cs
+++ client/Eldritch Duels/Assets/Scripts/Market Scene/BuyPack.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuyPack : MonoBehaviour
 {
     public GameObject succeedPanel;
     public GameObject failedPanel;
+    public Text moneyText;
 
 
     public void buy() 
     {
+        // TODO: check if money is enough to buy the pack
+
         bool succeed = true;
 
         if (succeed)
             succeedPanel.SetActive(true);
         else
             failedPanel.SetActive(true);
+
+        // TODO: make sure you update new money value to the server
+ 
+    }
+
+    // update money text field
+    void Update()
+    {
+        // constantly change money value
+        // TODO: update money from server
+        moneyText.text = "500";
     }
 }
