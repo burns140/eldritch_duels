cf293ac02edee2c6625fdc527c73c77b6f2da3a8
dhairyaa <amind@purdue.edu>
2020-03-26 15:50:10 -0500

    Re add music functionality

---
+++ client/Eldritch Duels/Assets/Scripts/Music Scripts/CheckMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckMusic : MonoBehaviour
+{
+    public AudioClip audioClip; // The audio clip supposed to be playing for the current scene;
+
+    void Awake()
+    {
+        Debug.Log("Checking if music playing is correct");
+
+        if(LoadMusic.Instance.gameObject.GetComponent<AudioSource>().clip != audioClip){
+            LoadMusic.Instance.gameObject.GetComponent<AudioSource>().clip = audioClip; // Change clip
+            LoadMusic.Instance.gameObject.GetComponent<AudioSource>().Play(); // Play the new clip
+        }
+    }
+
+}
