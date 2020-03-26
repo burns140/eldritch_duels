dad75413f9ead124ad6212804677f38c91fc1d68
cjmms <brandon.xyw@gmail.com>
2020-03-26 06:10:59 -0400

    update open pack
    show front image when clicking card
    Moved image folder to Resources folder
    Load sprite base on image address

---
+++ client/Eldritch Duels/Assets/Scripts/Open Pack/getNewCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getNewCard : MonoBehaviour
+{
+    public Image frontImage;
+
+
+    public void generateNewCard() 
+    {
+        // TODO: randomly generate a card
+
+        // Load a sprite from "Assets/Resources/images/migo.png"
+        // Do not add "Assets/Resources" and ".png"
+        // make sure images folder is under Resources
+        ChangeImage("images/migo");
+
+    }
+
+    public void ChangeImage(string newImageTitle)
+    {
+        frontImage.sprite = Resources.Load<Sprite>(newImageTitle);
+    }
+}
