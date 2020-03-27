e96095788b49d7dc8ecf893b125810e6ed62cfb7
dhairyaa <amind@purdue.edu>
2020-03-26 19:10:18 -0500

    Add search button

---
+++ client/Eldritch Duels/Assets/Scripts/Lobby Scene/GotoSearch.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using eldritch;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class GotoSearch : MonoBehaviour
+{
+    public void MovetoSearch()
+    {
+        SceneManager.LoadScene("SearchFriendsScene");
+    }
+}
