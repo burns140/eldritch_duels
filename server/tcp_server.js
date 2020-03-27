c89c6a9fb1e02e658b9d476452b9ae8d5bb17594
matson3 <matson.kevin@yahoo.com>
2020-03-27 13:25:12 -0400

    Fixed improper merge, which prevents the duel from working correctly.

--- server/tcp_server.js
+++ server/tcp_server.js
@@ -72,17 +72,17 @@ function dataHandler(data) {
                 case "resendVerify":                // resend verification email
                     Email.resendVerification(obj, sock);
                     break;
-                case "shareDeck":
-                    Decks.shareDeck(obj, sock);     // Share deck with another user
+                case "shareDeck":                   // Share deck with another user
+                    Decks.shareDeck(obj, sock);
                     break;
-                case "copySharedDeck":
-                    Decks.copySharedDeck(obj, sock);    // Copy a shared deck to my decks, allowing me to edit
+                case "copySharedDeck":              // Copy a shared deck to my decks, allowing me to edit
+                    Decks.copySharedDeck(obj, sock);
                     break;
                 case "logout":                          // Logout
                     playList.removeSocket(sock);
                     break;
                 case "enterQueue":                 // Enter matchmaking queue
-                    Queue.enterQueue(obj, sock);
+                    Queue.enterQueue(obj, sock, onClientConnected);
                     break;
                 case "blockUser":                   // Block a user
                     Block.blockUser(obj, sock);
@@ -93,8 +93,35 @@ function dataHandler(data) {
                 case "getBlockedUsers":             // Get array of blocked users for this user
                     Block.getBlockedUsers(obj, sock);
                     break;
-                case "getBlockedByUsers":            //Get array of the users who have blocked me
+                case "getBlockedByUsers":            // Get array of the users who have blocked me
                     Block.getBlockedByUsers(obj, sock);
+                    break;
+                case "sendFriendRequest":           // Send friend request to a user
+                    Friends.sendFriendRequest(obj, sock);
+                    break;
+                case "acceptFriendRequest":         // Accept a friend request
+                    Friends.acceptFriendRequest(obj, sock);
+                    break;
+                case "rejectFriendRequest":         // Reject a friend request
+                    Friends.rejectFriendRequest(obj, sock);
+                    break;
+                case "removeFriend":                // Remove a friend from my friends list
+                    Friends.removeFriend(obj, sock);
+                    break;
+                case "viewProfile":                 // Get info for a user's profile
+                    Profile.viewProfile(obj, sock);
+                    break;
+                case "reportPlayer":                // Report a player
+                    Profile.reportPlayer(obj, sock);
+                    break;
+                case "getAllFriends":               // Get a user's friends list
+                    Friends.getAllFriends(obj, sock);
+                    break;
+                case "getAllUsernames":             // Get all usernames that exist
+                    Friends.getAllUsernames(obj, sock);
+                    break;
+                case "openPack":                    // Open a pack
+                    Collection.openPack(obj, sock);
                     break;
                 default:                            // Command was invalid
                     sock.write('Not a valid command');
@@ -117,117 +144,7 @@ function onClientConnected(sock) {
 
     /* Determine what needs to be done every time
        data is received from a client */
-    sock.on('data', (data) => {       
-        try {     
-			const obj = JSON.parse(data);               // Turn data into a JSON object		
-            console.log(obj);
-            if (noTokenNeeded.includes(obj.cmd) || Verify.verify(obj.token, sock)) {        // Check that either no token is needed or the token is valid
-                switch (obj.cmd) {
-                    case "signup":                      // Signup new account
-                        Signup.signup(obj, sock);
-                        break;
-                    case "login":                       // Login
-                        Login.login(obj, sock);
-                        break;
-                    case "getAllDecks":                 // Get all deck names for an account
-                        Decks.getAllDecks(obj, sock);
-                        break;
-                    case "saveDeck":                    // Save a deck to an account
-                        Decks.saveDeck(obj, sock);      
-                        break;
-                    case "deleteDeck":                  // Delete a deck from an account
-                        Decks.deleteDeck(obj, sock);    
-                        break;
-                    case "getOneDeck":                  // Get a single deck object from an account
-                        Decks.getDeck(obj, sock);       
-                        break;
-                    case "getCollection":               // Get the entire collection for an account
-                        Collection.getCollection(obj, sock);    
-                        break;
-                    case "addCardToCollection":         // Add a card to an account's collection               
-                        Collection.addCard(obj, sock);
-                        break;
-                    case "removeCardFromCollection":    // Remove a card from an account's collection
-                        Collection.removeCard(obj, sock);
-                        break;
-                    case "tempPass":                    // Request an email with a temporary password
-                        Email.resetPassword(obj, sock);
-                        break;
-                    case "editProfile":                 // Edit a user's profile
-                        Profile.editProfile(obj, sock);
-                        break;
-                    case "changePassword":              // Change a user's password
-                        Profile.changePassword(obj, sock);
-                        break;
-                    case "changeEmail":                 // Change a user's email
-                        Profile.changeEmail(obj, sock);
-                        break;
-                    case "deleteAccount":               // Delete a user's account
-                        Profile.deleteAccount(obj, sock);
-                        break;
-                    case "resendVerify":                // resend verification email
-                        Email.resendVerification(obj, sock);
-                        break;
-                    case "shareDeck":                   // Share deck with another user
-                        Decks.shareDeck(obj, sock);     
-                        break;
-                    case "copySharedDeck":              // Copy a shared deck to my decks, allowing me to edit
-                        Decks.copySharedDeck(obj, sock);    
-                        break;
-                    case "logout":                          // Logout
-                        playList.removeSocket(sock);
-                        break;
-                    case "enterQueue":                 // Enter matchmaking queue
-                        Queue.enterQueue(obj, sock, onClientConnected);
-                        break;
-                    case "blockUser":                   // Block a user
-                        Block.blockUser(obj, sock);     
-                        break;
-                    case "unblockUser":                 // Unblock a user
-                        Block.unblockUser(obj, sock);   
-                        break;
-                    case "getBlockedUsers":             // Get array of blocked users for this user
-                        Block.getBlockedUsers(obj, sock);   
-                        break;
-                    case "getBlockedByUsers":            // Get array of the users who have blocked me
-                        Block.getBlockedByUsers(obj, sock);
-                        break;
-                    case "sendFriendRequest":           // Send friend request to a user
-                        Friends.sendFriendRequest(obj, sock);
-                        break;
-                    case "acceptFriendRequest":         // Accept a friend request
-                        Friends.acceptFriendRequest(obj, sock);
-                        break;
-                    case "rejectFriendRequest":         // Reject a friend request
-                        Friends.rejectFriendRequest(obj, sock);
-                        break;
-                    case "removeFriend":                // Remove a friend from my friends list
-                        Friends.removeFriend(obj, sock);
-                        break;
-                    case "viewProfile":                 // Get info for a user's profile
-                        Profile.viewProfile(obj, sock);
-                        break;
-                    case "reportPlayer":                // Report a player
-                        Profile.reportPlayer(obj, sock);
-                        break;
-                    case "getAllFriends":               // Get a user's friends list
-                        Friends.getAllFriends(obj, sock);
-                        break;
-                    case "getAllUsernames":             // Get all usernames that exist
-                        Friends.getAllUsernames(obj, sock);
-                        break;
-                    case "openPack":                    // Open a pack
-                        Collection.openPack(obj, sock);
-                        break;
-                    default:                            // Command was invalid
-                        sock.write('Not a valid command');
-                        break;
-                }
-            }
-        } catch (err) {
-            console.log(`tcp: ${err}`);
-        }
-    });
+    sock.on('data', dataHandler);
 
     /* There was a problem */
     sock.on('error', (err) => {
