f27537efd698af92dd3f0d9d2224ea1f62ae78e9
Stephen Burns <burns140@purdue.edu>
2020-03-29 14:03:17 -0400

    maybe prof pic

--- server/tcp_handling/signup.js
+++ server/tcp_handling/signup.js
@@ -102,7 +102,7 @@ const signup = (data, sock) => {
                         banLength: 0,                          // length of current tempBan
                         emailToChange: "",                   // Email they are trying to change to
                         reported: [],
-                        customArt: []
+                        customArt: ""
                     }).then(result => {
                         console.log(`User with email ${email} and id ${result.insertedId} successfully created`);
                         sock.write(`User successfully created with id ${result.insertedId}. You must verify your email before you can play`);
