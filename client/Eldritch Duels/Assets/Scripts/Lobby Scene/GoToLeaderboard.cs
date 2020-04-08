97d248807a9df0f81bcc382ae97300dd936bbc5e
dhairyaa <amind@purdue.edu>
2020-04-07 20:07:52 -0500

    Creating Leaderboard button on Lobby scene, and creating Leaderboard Scene

---
+++ client/Eldritch Duels/Assets/Scripts/Lobby Scene/GoToLeaderboard.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using eldritch;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class GoToLeaderboard : MonoBehaviour
+{
+    public void moveToLeaderboard()
+    {
+       SceneManager.LoadScene("Leaderboard"); 
+    }
+
+}
