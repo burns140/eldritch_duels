3b319b033162d2af1fa5492ceb9d69d8565c4994
nibarder <43069746+nibarder@users.noreply.github.com>
2020-04-30 17:40:32 -0400

    EXTREME AI

--- client/Eldritch Duels/Assets/Scripts/AI/AIScript.cs
+++ client/Eldritch Duels/Assets/Scripts/AI/AIScript.cs
@@ -38,7 +38,7 @@ namespace eldritch{
         public static AIDifficulty Difficulty = AIDifficulty.NORMAL;
         private static int AIRandomAdjust = 10; //higher the value, the less random; recommended range (-50, 50)
 
-        #region geleral AI 
+        #region general AI 
         public static List<Card> GetAILibrary(){
             List<Card> res = new List<Card>();
             string s = easyDecks[0];
@@ -77,9 +77,9 @@ namespace eldritch{
             }else if(Difficulty == AIDifficulty.NORMAL){
                 duelData.StartCoroutine(normalTurn(duelData));
             }else if(Difficulty == AIDifficulty.HARD){
-                hardTurn(duelData);
+                duelData.StartCoroutine(hardTurn(duelData));
             }else if(Difficulty == AIDifficulty.EXTREME){
-                extremeTurn(duelData);
+                duelData.StartCoroutine(hardTurn(duelData));
             }
 
         }
@@ -92,9 +92,9 @@ namespace eldritch{
             }else if(Difficulty == AIDifficulty.NORMAL){
                 normalBlock(duelData);
             }else if(Difficulty == AIDifficulty.HARD){
-                hardBlock(duelData);
+                blockHard(duelData);
             }else if(Difficulty == AIDifficulty.EXTREME){
-                extremeBlock(duelData);
+                blockHard(duelData);
             }
         }
 
@@ -352,32 +352,199 @@ namespace eldritch{
 
             #endregion
             //hard ai looks at both states
-            private static void hardTurn(AIDuel ad){
-                
+           
+
+            #region hard AI
+
+            public static IEnumerator hardTurn(AIDuel ad){
+                Debug.Log("AI starting...");
+                    //wait
+                    yield return new WaitForSeconds(.5f);
+
+                    //play cards
+                    Debug.Log("AI Casting...");
+                    int count = ad.oppState.inHand.Count;
+                    while(count > 0 && hardCast(ref ad)){
+                        Debug.Log(count);
+                        count--;
+                        yield return new WaitForSeconds(.5f);
+                        
+                    }
+                    
+
+                    //attack
+                    Debug.Log("AI attacking...");
+                    attackHard(ad);
+
+                    //to block
+                    ad.ToBlockPhase(); 
             }
-            //extreme ai is harder version of hard ai
-            private static void extremeTurn(AIDuel ad){
-                
+            private static bool hardCast(ref AIDuel ad){
+                //create weight list 
+                List<WeightContainer> wts = new List<WeightContainer>();
+                foreach(Card c in ad.oppState.inHand){
+                    WeightContainer wc;
+                    wc.card = c;
+                    wc.weight = normalWeight(ad, c);
+                    insertAdd(wc, SortType.WEIGHT, ref wts);
+                }
+                Debug.Log("AI First Choice: " + wts[0].card.CardName);
+                if(!PlayCard(ad, wts[0].card)){
+                    return false;
+                }
+                return true;
             }
 
-            
-        
-            
+            private static int hardWeight(AIDuel data, Card c){
+                float res = 0;
+                    res = (c.CardCost * Weights.MANA) + (turnsToCast(data.currentTurn, c.CardCost, data.oppState.mana) * Weights.TURN_DIFFERENCE) + (c.AttackPower * Weights.POWER) + (c.DefencePower * Weights.POWER) + Random.Range(0,3);
+                    if(data.oppState.hp < 20 && c.SpellType == CardType.SPELL){
+                        res += 1;
+                    }
+                    if(data.oppState.hp < 10 && c.SpellType == CardType.SPELL){
+                        res += 2;
+                    }
+                    if(c.CardCost == 0){
+                        res = -5; //AI will play 0 cost cards
+                    }
+                    if(c.HasFly){
+                        res -= 1;
+                    }
+                    return (int)res;
+            }
 
+            private static void attackHard(AIDuel aIDuel){
+                    //create list
+                    List<WeightContainer> wts = new List<WeightContainer>();
 
+                    if(aIDuel.myState.onField.Count == 0){
+                        //attack with all
+                        foreach(Card c in aIDuel.oppState.onField){                        
+                            aIDuel.addOppAttacker(c.CardName);                        
+                        }
+                        return;
+                    }
+                    //attack all fliers
+                    foreach(Card c in aIDuel.oppState.onField){
+                        if(c.HasFly){
+                            aIDuel.addOppAttacker(c.CardName);
+                        }
+                    }
+                    foreach(Card c in aIDuel.oppState.onField){
+                        WeightContainer wc;
+                        wc.card = c;
+                        wc.weight = c.AttackPower;
+                        if(!c.HasFly)
+                            insertAdd(wc, SortType.WEIGHT, ref wts);
+                    }
+                    int maxAttack = Random.Range(wts.Count/2, wts.Count);
+                    for(int i = 0; i < maxAttack; i++){
+                        aIDuel.addOppAttacker(wts[wts.Count-i-1].card.CardName);
+                    }
+
+                }
+
+                private static void blockHard(AIDuel ad){
+                    Debug.Log("AI Blocking...");
+                    //make lists
+                    List<WeightContainer> attackers = new List<WeightContainer>();
+                    List<WeightContainer> flyAttackers = new List<WeightContainer>();
+                    List<WeightContainer> blockers = new List<WeightContainer>();
+
+                    
+                    
+                    //get attackers
+                    foreach(AttackBlock ab in ad.attackers){
+                        WeightContainer wc;
+                        if(ab.attackCard.HasFly){
+                            wc.card = ab.attackCard;
+                            wc.weight = 0;
+                            insertAdd(wc, SortType.POWER, ref flyAttackers);
+                        }else{
+                            wc.card = ab.attackCard;
+                            wc.weight = 0;
+                            insertAdd(wc, SortType.POWER, ref attackers);
+                        }
+                    }
+                    //get blockers
+                    foreach(Card c in ad.oppState.onField){
+                        WeightContainer wc;
+                        wc.card = c;
+                        wc.weight = 0;
+                        insertAdd(wc, SortType.DEFENCE, ref blockers);
+
+                    }
+                    //block strongest fliers with weakest
+                    int blocked = 0;
+                    int count = flyAttackers.Count-1;
+                    for(int j = count; j >= 0;j--){
+                        for(int i = 0; i < blockers.Count; i++){
+                            if(blockers[i].card.HasFly && stratBlock(flyAttackers[count].card, blockers[i].card)){
+                                ad.addOppBlocker(flyAttackers[count].card.CardName + "-" + blockers[i].card.CardName);
+                                blockers.RemoveAt(i);
+                                blocked++;
+                                break;
+                            }
+                        }
+
+                        count--;
+                    }
+                    //block the srongest flying creature with weakest
+                    if(flyAttackers.Count > 0){
+                        for(int i = 0; i < blockers.Count; i++){
+                            if(blockers[i].card.HasFly && flyAttackers[flyAttackers.Count-1].card.AttackPower > 3){
+                                ad.addOppBlocker(flyAttackers[flyAttackers.Count-1].card.CardName + "-" + blockers[i].card.CardName);
+                                blockers.RemoveAt(i);
+                                break;
+                            }
+                        }
+                    }
+                    //block strongest attackers
+                    blocked = 0;
+                    for(int j = 0; j < attackers.Count;j++){
+                        if(blockers.Count == 0){
+                            break;
+                        }
+                        for(int i = 0; i < blockers.Count; i++){
+                            if(stratBlock(attackers[j].card, blockers[i].card)){
+                                ad.addOppBlocker(attackers[j].card.CardName + "-" + blockers[i].card.CardName);
+                                blockers.RemoveAt(i);
+                                blocked++;
+                                break;
+                            }
+                        }                        
+                    }
+                    if(blocked == 0 && blockers.Count > 0 && attackers[attackers.Count-1].card.AttackPower > 3){
+                        ad.addOppBlocker(attackers[attackers.Count-1].card.CardName + "-" + blockers[0].card.CardName);
+                    }
+                }
+
+                private static int attackPotential(PlayerState state){
+                    int res = 0;
+                    foreach(Card c in state.onField){
+                        res += c.AttackPower;
+                    }
+                    return res;
+                }
+
+                private static bool stratBlock(Card a, Card b){
+                    return a.AttackPower < b.DefencePower || a.DefencePower == 1 || a.DefencePower == b.AttackPower;
+                }
+
+            #endregion
             
-
-            
-
 
         #endregion
 
 
         #region library lists
-            private static string[] easyDecks = new string[] {"Mi_Go-20,Blood Vial-12"};
-            private static string[] normalDecks = new string[] {"Mi_Go-20,Blood Vial-12"};
-            private static string[] hardDecks = new string[] {"Mi_Go-20,Blood Vial-12"};
-            private static string[] extremeDecks = new string[] {"Mi_Go-20,Blood Vial-12"};
+            private static string[] easyDecks = new string[] {"Mi_Go-20,Blood Vial-12",
+                                                                "Mi_Go-20,Mi_Go Worker-12"};
+            private static string[] normalDecks = new string[] {"Mi_Go-10,Mi_Go Worker-10,Blood Vial-7,Madman's Knowledge-5"};
+            private static string[] hardDecks = new string[] {"Mi_Go Worker-10,Blood Vial-8,Madman's Knowledge-2,Great One's Wisdom-1",
+                                                                    "Mi_Go Worker-10,Mi_Go Queen-4,Mi_Go Zombie-8,Mi_Go-10"};
+            private static string[] extremeDecks = new string[] {"Great One's Wisdom-4,Madman's Knowledge-10,Mi_Go Queen-4,Mi_Go Zombie-6,Moon Presence-2,Lady Maria-4,Pungent Blood Cocktail-2",
+                                                                    "Great One's Wisdom-4,Madman's Knowledge-10,Mi_Go Queen-4,Mi_Go Zombie-6,Chime Maiden-5,Mi_Go Worker-3"};
         #endregion
         
     }
